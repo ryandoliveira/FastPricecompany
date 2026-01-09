e3edd0ee8a6c46390ce6d1ae3b6513ba91d21b59
Ryan Monsores De Oliveira <149541255+ryandoliveira@users.noreply.github.com>
2026-01-08 21:28:19 -0300

    Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,7 +18,8 @@ function Footer() {
             <h4>Equipe Front-end</h4>
             <div className="team-members">
               <div className="member">
-                <img src={devryan} alt="Ryan" className="team-image" />
+                <img src= "0905A881-9019-4D9A-ACA7-1AAF47581BEB.jpg
+" alt="Ryan" className="team-image" />
                 <p className="member-role">Idealização, prototipagem e criação do layout e suas ferramentas.</p>
                 <a href="https://www.linkedin.com/in/ryan-oliveira-2b54092a1/" target="_blank" rel="noopener noreferrer" className="linkedin-button">
                   <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" /> Ryan Oliveira
@@ -81,5 +82,6 @@ function Footer() {
     </footer>
   );
 }
+
 
 export default Footer;
